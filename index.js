f6dfc4a1dd7fb4d2406b36c21f4cff7ef50ea1ae
Alex Karanja <alexnjorge953@gmail.com>
2024-04-04 15:43:40 +0300

    changes

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-// Code your solution in this file!
+function distanceFromHqInBlocks(pickupLocation) {
+    const hqLocation =42;
+    return Math.abs(pickupLocation-hqLocation);
+}
+//function to calculate the distance in feet 
+const feetperblock = 264;
+
+function distanceFromHqInFeet (pickupLocation) {
+    const blocks =distanceFromHqInBlocks(pickupLocation);
+    const feet = blocks * feetperblock
+    return feet;
+}
+function distanceTravelledInFeet (startblock, endblock) {
+    return maths.abs(endblock - startblock) npm *264;
+}
